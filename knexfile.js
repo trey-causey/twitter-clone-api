87cf0935f372249d48c1249f389c0ae10b4fbbd0
trey-causey <trey.causey@outlook.com>
2020-02-26 14:08:52 -0500

    added localhost postgres info and commented out heroku

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,23 @@
 // Update with your config settings.
 const pg = require("pg");
-        pg.defaults.ssl = false;
+        pg.defaults.ssl = true;
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: { 
-      URI: "postgres://auykhabvswezzt:f961ca9094fc34f0b79f9b8f169ef0395532071701ccd0a2e1c1dabd0160e95b@ec2-52-86-33-50.compute-1.amazonaws.com:5432/d1bp24nhp6dpam",
-      host: 'ec2-52-86-33-50.compute-1.amazonaws.com',
-      database: 'd1bp24nhp6dpam',
-      user: 'auykhabvswezzt',
-      password: "f961ca9094fc34f0b79f9b8f169ef0395532071701ccd0a2e1c1dabd0160e95b"
+    // connection: { 
+    //   URI: "postgres://auykhabvswezzt:f961ca9094fc34f0b79f9b8f169ef0395532071701ccd0a2e1c1dabd0160e95b@ec2-52-86-33-50.compute-1.amazonaws.com:5432/d1bp24nhp6dpam",
+    //   host: 'ec2-52-86-33-50.compute-1.amazonaws.com',
+    //   database: 'd1bp24nhp6dpam',
+    //   user: 'auykhabvswezzt',
+    //   password: "f961ca9094fc34f0b79f9b8f169ef0395532071701ccd0a2e1c1dabd0160e95b" }
+
+    connection: {
+      host: 'localhost',
+      database: 'twitter_clone',
+      user: 'twitter_user',
+      password: 'Passw0rd!'
+
     }
   },
 
