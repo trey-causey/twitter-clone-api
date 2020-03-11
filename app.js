9b0e175c6e802049efb483ad576438a8b2e56e68
trey-causey <trey.causey@outlook.com>
2020-03-11 19:40:38 -0400

    lots of changes...still looking at heroku

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ const knex = require("knex")(development);
 
 const app = express();
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', async (req, res) => {
   const posts = await knex("posts");
   res.send(posts);
+})
+
+app.post("/tweet", async (req, res) => {
+  console.log(req.body);
+
+  await knex("posts").insert({
+    title: req.body.title,
+    body: req.body.tweet
+  });
+
+  res.send("I got your tweet!");
 })
 
 const port = process.env.PORT || 5000
